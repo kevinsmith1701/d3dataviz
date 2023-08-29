b2e7b31a0b563b7b0e8c5d5df7abaac78d25e51e
Kevin Smith <kevinasmith1701@gmail.com>
2023-08-29 07:49:43 -0700

    Ch5 - Added scales to bar chart

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,85 +1,130 @@
-var bardata = [];
-    for (var i = 0; i<50; i++) {
-      bardata.push(Math.random() * 30);
-    }
-var height = 400,
-    width = 600,
-    barWidth = 50,
-    barOffset = 5,
-    tempColor;
+d3.json("js/data/forecast.json", function (d) {
+  var temperatures = [],
+    dates = [],
+    margin = { top: 0, right: 0, bottom: 30, left: 20 },
+    height = 400 - margin.top - margin.bottom,
+    width = 600 - margin.left - margin.right;
 
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(bardata)])
-    .range([0,height]);
+  var tempColor,
+    yScale,
+    xScale,
+    colors,
+    tooltip,
+    myChart,
+    yAxisValues,
+    yAxisTicks,
+    xAxisValues,
+    xAxisTicks,
+    yGuide,
+    xGuide;
 
-var xScale = d3.scaleBand()
-    .domain(bardata)
-    .paddingInner(.3)
-    .paddingOuter(.1)
-    .range([0, width])
+  for (var i = 0; i < d.list.length; i++) {
+    temperatures.push(d.list[i].main.temp);
+    dates.push(new Date(d.list[i].dt_txt));
+  }
 
-var colors = d3.scaleLinear()
-    .domain([0, bardata.length *.33,
-                bardata.length *.66,
-                bardata.length
-                ])
-    .range(['#B58929', '#C61C6F',
-            '#268BD2', '#85992C'])
+  console.log(dates);
 
-var tooltip = d3.select('body')
-                .append('div')
-                .style('position', 'absolute')
-                .style('padding', '0 10px')
-                .style('background', 'white')
-                .style('opacity', 0);
+  yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(temperatures)])
+    .range([0, height]);
 
-var myChart = 
-d3.select('#viz').append('svg')
-  .attr('width', width)
-  .attr('height', height)
-.selectAll('rect').data(bardata)
-  .enter().append('rect')
-    .attr('fill', function(d, i) {
-      return colors(i)
-    })
-    .attr('width', function(d) {
+  yAxisValues = d3
+    .scaleLinear()
+    .domain([0, d3.max(temperatures)])
+    .range([height, 0]);
+  yAxisTicks = d3.axisLeft(yAxisValues).ticks(10);
+
+  xScale = d3
+    .scaleBand()
+    .domain(temperatures)
+    .paddingInner(0.3)
+    .paddingOuter(0.1)
+    .range([0, width]);
+
+  xAxisValues = d3
+    .scaleTime()
+    .domain([dates[0], dates[dates.length - 1]])
+    .range([0, width]);
+  xAxisTicks = d3.axisBottom(xAxisValues).ticks(d3.timeDay.every(1));
+
+  colors = d3
+    .scaleLinear()
+    .domain([0, 65, d3.max(temperatures)])
+    .range(["#FFFFFF", "#2D8BCF", "#DA3637"]);
+
+  tooltip = d3
+    .select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("padding", "0 10px")
+    .style("background", "white")
+    .style("opacity", 0);
+
+  myChart = d3
+    .select("#viz")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.right + ")")
+    .selectAll("rect")
+    .data(temperatures)
+    .enter()
+    .append("rect")
+    .attr("fill", colors)
+    .attr("width", function (d) {
       return xScale.bandwidth();
     })
-    .attr('height', 0)
-    .attr('x', function(d) {
+    .attr("height", 0)
+    .attr("x", function (d) {
       return xScale(d);
     })
-    .attr('y', height)
-    
-    .on('mouseover', function(d) {
+    .attr("y", height)
 
-      tooltip.transition().duration(200)
-        .style('opacity', .9)
+    .on("mouseover", function (d) {
+      tooltip.transition().duration(200).style("opacity", 0.9);
 
-      tooltip.html(d)
-        .style('left', (d3.event.pageX -35) + 'px')
-        .style('top', (d3.event.pageY -30) + 'px')
+      tooltip
+        .html(
+          '<div style="font-size: 2rem; font-weight:bold">' + d + "&deg;</div>"
+        )
+        .style("left", d3.event.pageX - 35 + "px")
+        .style("top", d3.event.pageY - 30 + "px");
 
       tempColor = this.style.fill;
-      d3.select(this)
-        .style('fill', 'yellow')
+      d3.select(this).style("fill", "yellow");
     })
 
-    .on('mouseout', function(d) {
-      d3.select(this)
-        .style('fill', tempColor)
+    .on("mouseout", function (d) {
+      tooltip.html("");
+      d3.select(this).style("fill", tempColor);
     });
 
+  yGuide = d3
+    .select("#viz svg")
+    .append("g")
+    .attr("transform", "translate(20,0)")
+    .call(yAxisTicks);
 
-myChart.transition()
-  .attr('height', function(d) {
-    return yScale(d);
-  })
-  .attr('y', function(d) {
-    return height - yScale(d);
-  })
-  .delay(function(d, i) {
-    return i * 20;
-  })
-  .duration(1000)
-  .ease(d3.easeBounceOut)
+  xGuide = d3
+    .select("#viz svg")
+    .append("g")
+    .attr("transform", "translate(20," + height + ")")
+    .call(xAxisTicks);
+
+  myChart
+    .transition()
+    .attr("height", function (d) {
+      return yScale(d);
+    })
+    .attr("y", function (d) {
+      return height - yScale(d);
+    })
+    .delay(function (d, i) {
+      return i * 20;
+    })
+    .duration(1000)
+    .ease(d3.easeBounceOut);
+}); // json import
